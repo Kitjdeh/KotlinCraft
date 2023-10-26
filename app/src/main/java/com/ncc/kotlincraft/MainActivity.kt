ce41d890485f70c9dc21fd06f12b4b999f5c1855
Kitjdeh <rltjdeh@naver.com>
2023-10-26 22:57:16 +0900

    list_order 삭제, expression으로 대체

--- app/src/main/java/com/ncc/kotlincraft/MainActivity.kt
+++ app/src/main/java/com/ncc/kotlincraft/MainActivity.kt
@@ -11,10 +11,17 @@ import java.util.Stack
 
 class MainActivity : AppCompatActivity() {
 
+    //expression : 중위 표현식
     private var expression = ""
-    private val resultStack = Stack<Int>()
+
+    //후위 표현식
+    private val resultStack = Stack<Double>()
+
+    //후위 표현식
     private val postFixStack = Stack<String>()
+
     private val listOrder = mutableListOf<String>()
+
     var totalNumber = ""
     val stack = Stack<String>()
     var num = ""
@@ -41,7 +48,7 @@ class MainActivity : AppCompatActivity() {
         val multiplyBtn = findViewById<TextView>(R.id.multiplyBtn)
         val divideBtn = findViewById<TextView>(R.id.divideBtn)
         val clearBtn = findViewById<TextView>(R.id.clearBtn)
-        val plusMinusBtn = findViewById<TextView>(R.id.plus_minusBtn)
+        val deleteBtn = findViewById<TextView>(R.id.deletBtn)
         val calculatorBtn = findViewById<TextView>(R.id.equalBtn)
         val leftParenthesis = findViewById<TextView>(R.id.left_parenthesis)
         val rightParenthesis = findViewById<TextView>(R.id.right_parenthesis)
@@ -98,6 +105,10 @@ class MainActivity : AppCompatActivity() {
             result.text = expression
         }
 
+        deleteBtn.setOnClickListener {
+            expression = expression.substring(0, -1)
+            result.text = expression
+        }
         clearBtn.setOnClickListener {
             expression = ""
             num = ""
@@ -108,51 +119,51 @@ class MainActivity : AppCompatActivity() {
         }
         plusBtn.setOnClickListener {
             expression += "+"
-            if (num.isNotEmpty()) {
-                listOrder.add(num)
-            }
-            listOrder.add("+")
+//            if (num.isNotEmpty()) {
+//                listOrder.add(num)
+//            }
+//            listOrder.add("+")
             result.text = expression
             num = ""
         }
         minusBtn.setOnClickListener {
             expression += "-"
-            if (num.isNotEmpty()) {
-                listOrder.add(num)
-            }
-            listOrder.add("-")
+//            if (num.isNotEmpty()) {
+//                listOrder.add(num)
+//            }
+//            listOrder.add("-")
             result.text = expression
             num = ""
         }
         multiplyBtn.setOnClickListener {
             expression += "*"
-            if (num.isNotEmpty()) {
-                listOrder.add(num)
-            }
-            listOrder.add("*")
+//            if (num.isNotEmpty()) {
+//                listOrder.add(num)
+//            }
+//            listOrder.add("*")
             result.text = expression
             num = ""
         }
         divideBtn.setOnClickListener {
             expression += "/"
-            if (num.isNotEmpty()) {
-                listOrder.add(num)
-            }
-            listOrder.add("/")
+//            if (num.isNotEmpty()) {
+//                listOrder.add(num)
+//            }
+//            listOrder.add("/")
             result.text = expression
             num = ""
         }
         leftParenthesis.setOnClickListener {
             expression += "("
-            listOrder.add("(")
-            result.text = expression
+//            listOrder.add("(")
+//            result.text = expression
 //            num = ""
         }
         rightParenthesis.setOnClickListener {
             expression += ")"
-            listOrder.add(num)
-            listOrder.add(")")
-            num = ""
+//            listOrder.add(num)
+//            listOrder.add(")")
+//            num = ""
             result.text = expression
         }
 
@@ -167,42 +178,63 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    //중위 표현식 expression을 후위표현식으로 변환
     private fun postFix() {
-        Log.d("입력값", listOrder.toString())
-        for (num in listOrder) {
-            if (num.isDigitsOnly()) {
-                postFixStack.add(num)
-            } else if (num == "(") {
-                stack.add(num)
-            } else if (num == ")") {
-                while (stack.isNotEmpty() && stack.last() != "(") {
-                    postFixStack.add(stack.pop())
+        // 2자리수 이상의 string 일경우 words에 포함시켜서 진행
+        var words = ""
+        for (word in expression) {
+            if (word.isDigit() || word.equals(".")) {
+                //.이 이미 있다면 break
+                if (words.contains(".")){
+                    Toast.makeText(this, "잘못된 수식입니다.", Toast.LENGTH_SHORT).show()
+                    break
                 }
-                stack.pop()
-            } else if (num == "*" || num == "/") {
-                while (stack.isNotEmpty() && (stack.last() == "*" || stack.last() == "/")) {
-                    postFixStack.add(stack.pop())
-                }
-                stack.add(num)
-            } else if (num == "+" || num == "-") {
-                while (stack.isNotEmpty() && stack.last() != "(") {
-                    postFixStack.add(stack.pop())
-                }
-                stack.add(num)
+                words += word
             }
+            else {
+                //isDigt가 아니다 -> 연산 or ()이니 계산된 words 를 넣어준다.
+                if (words.isNotEmpty()) {
+                    stack.add(words)
+                    words = ""
+                }
+                if (word.equals("(")) {
+                    stack.add(word.toString())
+                }
+
+                // ")" 가 나올 경우 해당 )과 맞아 떨어지는 (이 나오기 전까지 stack들을 postFixStack에 집어넣는다.(앞자리로 보내서 먼저 계산 할 수 있도록 만든다.)
+                else if (word.equals(")")) {
+                    while (stack.isNotEmpty() && stack.last() != "(") {
+                        postFixStack.add(stack.pop())
+                    }
+                    // while 이 종료되었다면 stack의 맨뒤가 ( 이거나 stack이 비어있음 -> pop()로 정리
+                    stack.pop()
+                } else if (word.equals("*") || word.equals("/")) {
+
+                    // *과 / 가 나올경우 이전에 나온 *나 /가 먼저기 때문에 해당 값들을 후위 표현식에 넣어준다.
+                    while (stack.isNotEmpty() && (stack.last() == "*" || stack.last() == "/")) {
+                        postFixStack.add(stack.pop())
+                    }
+                    stack.add(word.toString())
+                } else if (word.equals("+") || word.equals("-")) {
+                    // +와 - 는 우선순이가 제일 낮기 때문에 () 기준으로 가장 뒤에 가도록 후위 표현식에 배치한다.
+                    while (stack.isNotEmpty() && stack.last() != "(") {
+                        postFixStack.add(stack.pop())
+                    }
+                    stack.add(word.toString())
+                }
+            }
+            while (stack.isNotEmpty()) {
+                postFixStack.add(stack.pop())
+            }
+            calculateStack()
         }
-        Log.d("listorder 순회 끝", stack.toString())
-        while (stack.isNotEmpty()) {
-            postFixStack.add(stack.pop())
-        }
-        calculateStack()
     }
 
     private fun calculateStack() {
         loop@ for (num in postFixStack) {
 //        while (num in postFixStack){
             if (num.isDigitsOnly()) {
-                resultStack.add(num.toInt())
+                resultStack.add(num.toDouble())
             } else if (num == "+") {
                 val secondNum = resultStack.pop()
                 val firstNum = resultStack.pop()
