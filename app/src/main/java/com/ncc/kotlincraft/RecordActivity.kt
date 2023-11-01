dd419a92c7e98c20fdd943e0ad8506ceb71d7de2
Kitjdeh <rltjdeh@naver.com>
2023-11-01 21:49:14 +0900

    롱클릭 삭제 구현

--- app/src/main/java/com/ncc/kotlincraft/RecordActivity.kt
+++ app/src/main/java/com/ncc/kotlincraft/RecordActivity.kt
@@ -1,11 +1,14 @@
 package com.ncc.kotlincraft
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
 import androidx.appcompat.widget.AppCompatButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,14 +19,15 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class RecordActivity : AppCompatActivity() {
+class RecordActivity : AppCompatActivity(
+
+
+) {
 
     var db: RecordDatabase? = null
 
     // 받아올 records를 사용할 record List 생성
-    var records = arrayListOf<Record>(
-
-    )
+    var records = arrayListOf<Record>()
 
     @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,11 +44,10 @@ class RecordActivity : AppCompatActivity() {
         CoroutineScope(Dispatchers.IO).launch {
             val recordDao = db!!.recordDao()
             records = recordDao.getAll() as ArrayList<Record>
-
             //백그라운드 스레드 작업이 끝났으니 main스레드 에서 UI 변경 작업 진행
             withContext(Dispatchers.Main) {
                 // 어댑터 연결
-                rv_record.adapter = RecordAdapter(records)
+                rv_record.adapter = RecordAdapter(records) { record -> deleteRecord(record) }
             }
 //            //records가 채워 졌으니 adapter와  연결
 //            var recordAdapter = RecordAdapter(records)
@@ -61,14 +64,36 @@ class RecordActivity : AppCompatActivity() {
         }
     }
 
+    //Adapter에서 실행 할 수 있게 세팅
+    private fun deleteRecord(record: Record) {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("삭제하시겠습니까?")
+            .setPositiveButton(
+                "삭제",
+                DialogInterface.OnClickListener { dialog, which ->
+                    //백그라운드 스레드에서 데이터 삭제
+                    CoroutineScope(Dispatchers.IO).launch {
+                        val recordDao = db!!.recordDao()
+                        recordDao.deleteRecord(record)
+                        records = recordDao.getAll() as ArrayList<Record>
+                    }
+
+                }
+            )
+            .setNegativeButton("취소", null)
+        builder.show()
+    }
+
     private fun initRecyclerView() {
         val rv_record = findViewById<RecyclerView>(R.id.rv_record)
         //records가 채워 졌으니 adapter와  연결
-        var recordAdapter = RecordAdapter(records)
+        var recordAdapter = RecordAdapter(records) { record -> deleteRecord(record) }
+//        var recordAdapter = RecordAdapter(records)
         // 어댑터 연결
         rv_record.adapter = recordAdapter
         // 어댑터의 layoutmanager 연결
         rv_record.layoutManager = LinearLayoutManager(this@RecordActivity)
+
     }
 
 }
--- app/src/main/java/com/ncc/kotlincraft/RecordAdapter.kt
+++ app/src/main/java/com/ncc/kotlincraft/RecordAdapter.kt
@@ -8,14 +8,16 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class RecordAdapter(val itemList: List<Record>): RecyclerView.Adapter<RecordAdapter.ViewHolder>()
-{
-    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+class RecordAdapter(val itemList: List<Record>, private val clickListener: (Record) -> Unit) :
+    RecyclerView.Adapter<RecordAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val expression = itemView.findViewById<TextView>(R.id.rv_item)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecordAdapter.ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.record_rv_item,parent,false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.record_rv_item, parent, false)
         return ViewHolder(view)
     }
 
@@ -23,31 +25,38 @@ class RecordAdapter(val itemList: List<Record>): RecyclerView.Adapter<RecordAdap
         holder.expression.text = itemList[position].expression
         // = 기준으로 결과 값 변경
         val result = itemList[position].expression!!.split("=").last()
-        if (result.toDouble().toInt() > 0){
-            when (val number = result.toDouble().toInt() ) {
+        if (result.toDouble().toInt() > 0) {
+            when (val number = result.toDouble().toInt()) {
                 in 0..10 -> {
                     holder.expression.setBackgroundColor(Color.YELLOW)
                 }
+
                 in 11..100 -> {
 
                     holder.expression.setBackgroundColor(Color.GREEN)
                 }
+
                 in 101..1000 -> {
 
                     holder.expression.setBackgroundColor(Color.RED)
                 }
-                else -> {
 
+                else -> {
                     holder.expression.setBackgroundColor(Color.BLUE)
                 }
             }
-
         }
 
+        //받아온 clickListner함수에 해당 itemList 주입
+        holder.itemView.setOnLongClickListener {
+            clickListener(itemList[position])
+//            Log.d("롱클릭",itemList[position].expression.toString())
+            return@setOnLongClickListener true
+        }
     }
 
     override fun getItemCount(): Int {
-       return itemList.count()
+        return itemList.count()
     }
 
 }
