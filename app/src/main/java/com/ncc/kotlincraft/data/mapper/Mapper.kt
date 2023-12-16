6ee099cec34ec05ae56fc3468cdbe5cc13eb8ae6
Kitjdeh <rltjdeh@naver.com>
2023-12-16 19:11:17 +0900

    mapper 확장함수로 변경

--- app/src/main/java/com/ncc/kotlincraft/data/mapper/Mapper.kt
+++ app/src/main/java/com/ncc/kotlincraft/data/mapper/Mapper.kt
@@ -3,19 +3,37 @@ package com.ncc.kotlincraft.data.mapper
 import com.ncc.kotlincraft.data.entity.Record
 import com.ncc.kotlincraft.domain.model.DomainRecord
 
-fun mapperDomainToData(records: DomainRecord): Record {
-    return Record(
-        records.id,
-        records.expression
-    )
-
-}
-
-fun mapperDataToDomain(records: List<Record>): List<DomainRecord> {
-    return records.toList().map {
+fun List<Record>.mapperToDomainRecord(): List<DomainRecord> {
+    return this.toList().map {
         DomainRecord(
             it.id,
             it.expression
         )
     }
 }
+
+fun DomainRecord.mapperToRecord(): Record {
+    return  Record(
+        this.id,
+        this.expression
+    )
+
+}
+
+//fun mapperDomainToData(records: DomainRecord): Record {
+//    return Record(
+//        records.id,
+//        records.expression
+//    )
+//
+//}
+
+//fun mapperDataToDomain(records: List<Record>): List<DomainRecord> {
+//    return records.toList().map {
+//        DomainRecord(
+//            it.id,
+//            it.expression
+//        )
+//    }
+//}
+
--- app/src/main/java/com/ncc/kotlincraft/data/repository/RecordRepositoryImpl.kt
+++ app/src/main/java/com/ncc/kotlincraft/data/repository/RecordRepositoryImpl.kt
@@ -1,11 +1,8 @@
 package com.ncc.kotlincraft.data.repository
 
-import android.util.Log
-import com.ncc.kotlincraft.data.db.RecordDatabase
-import com.ncc.kotlincraft.data.entity.Record
-import com.ncc.kotlincraft.data.mapper.mapperDataToDomain
-import com.ncc.kotlincraft.data.mapper.mapperDomainToData
-import com.ncc.kotlincraft.data.repository.local.RecordDataSource
+
+import com.ncc.kotlincraft.data.mapper.mapperToDomainRecord
+import com.ncc.kotlincraft.data.mapper.mapperToRecord
 import com.ncc.kotlincraft.data.repository.local.RecordDataSourceImpl
 import com.ncc.kotlincraft.domain.model.DomainRecord
 import com.ncc.kotlincraft.domain.repository.RecordRepository
@@ -16,18 +13,18 @@ class RecordRepositoryImpl
 
     private val recordDataSource = RecordDataSourceImpl()
     override fun getRecords(): List<DomainRecord> {
-        return mapperDataToDomain(recordDataSource.getRecord())
+        return recordDataSource.getRecord().mapperToDomainRecord()
     }
 
     override fun changeRecord(start: DomainRecord, end: DomainRecord) {
-        recordDataSource.changeRecord(mapperDomainToData(start), mapperDomainToData(end))
+        recordDataSource.changeRecord(start.mapperToRecord(), end.mapperToRecord())
     }
 
     override fun writeRecord(record: DomainRecord) {
-        recordDataSource.writeRecord(mapperDomainToData(record))
+        recordDataSource.writeRecord(record.mapperToRecord())
     }
 
     override fun deleteRecord(record: DomainRecord): List<DomainRecord> {
-        return mapperDataToDomain(recordDataSource.deleteRecord(mapperDomainToData(record)))
+        return recordDataSource.deleteRecord(record.mapperToRecord()).mapperToDomainRecord()
     }
 }
